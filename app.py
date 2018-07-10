c0089091b5e7921423b9f3835fa38e64d3b75406
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2018-07-10 21:49:04 +0600

    "upgrade"

--- app.py
+++ app.py
@@ -1,127 +1,58 @@
-﻿import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory, session
 import pandas as pd
+from werkzeug.utils import secure_filename
 
-import io
-import base64
-
-app = dash.Dash()
-
-app.scripts.config.serve_locally = True
-app.config['suppress_callback_exceptions'] = True
-
-app.layout = html.Div([
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-    html.Div(id='output-data-upload'),
-    html.Div(id='graph')
-])
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-    decoded = base64.b64decode(content_string)
-    global file_name 
-    file_name = filename
-    try:
-        if 'csv' in filename:
-            global df
-            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-            column = list(df)
-        elif 'xls' in filename:
-            df = pd.read_excel(io.BytesIO(decoded))
-            column = list(df)
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return html.Div([
-        html.Div([
-        html.Div([
-        html.Div('Select X-Axis:'),
-        dcc.Dropdown(
-            id='xaxis',
-            options = [ {'label': i, 'value': i} for i in column],
-        ),
-        ],
-        style={'width': '35%', 'display': 'inline-block'}),
-
-        html.Div([
-        html.Div('Select Y-Axis:'),
-        dcc.Dropdown(
-            id='yaxis',
-            options = [ {'label': i, 'value': i} for i in column],
-        ),
-        ],
-        style={'width': '35%', 'display': 'inline-block'}),
-
-        html.Div([
-        html.Div('Select Graph Type:'),
-        dcc.Dropdown(
-            id='type',
-            options = [{'label': 'line', 'value': 'line'},
-                       {'label': 'bar', 'value': 'bar'}],
-            value='line'
-        ),
-        ],
-        style={'width': '30%', 'display': 'inline-block'}),
-        ]),
-    ])
+UPLOAD_FOLDER = 'static/'
+ALLOWED_EXTENSIONS = set(['csv'])
 
 
-@app.callback(Output('output-data-upload', 'children'),
-              [Input('upload-data', 'contents'),
-               Input('upload-data', 'filename'),
-               Input('upload-data', 'last_modified')])
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
+app = Flask(__name__)
+# Setting secret key for flask session
+app.secret_key = b'unlock'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#Checking if the uploaded file is allowed
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET','POST'])
+def upload(filename=None):
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            flash("No selected file")
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #creating session of filename
+            if filename:
+                session["filename"] = filename
+
+    return render_template('index.html', filename=filename)
+
+@app.route('/graph/', methods=['GET', 'POST'])
+def graph(xaxis=None, yaxis=None, data=None, column=None):
+    if request.method == 'POST':
+        xaxis = request.form['x_data']
+        yaxis = request.form['y_data']
+
+    filename = session.get('filename', None)
+
+    if filename:
+        df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        column = list(df)
+        data = [list(df[d]) for d in column]
+
+    return render_template('graph.html', xaxis=xaxis, yaxis=yaxis, column=column, data=data)
 
 
-@app.callback(
-    Output('graph', 'children'),
-    [Input('xaxis', 'value'),
-     Input('yaxis','value'),
-     Input('type','value')])
-def update_graph(xaxis, yaxis, type):
-    return dcc.Graph(
-        id='graph_bddata',
-        figure={
-            'data':[
-                {'x':list(df[xaxis]),'y':list(df[yaxis]),'type':type,'name':'Population'},
-            ],
-            'layout':{
-                'title':file_name,
-            },
-        }
-    )
-
-app.css.append_css({
-    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-})
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run(debug = True)
